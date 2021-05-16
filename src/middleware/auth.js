1f4353bc03a8f2e037fc3a12f1c10fd9a50eb3a6
Vaibhav Shinde <66087317+darkshadow56@users.noreply.github.com>
2021-05-16 15:20:00 +0530

    Update auth.js

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -6,7 +6,7 @@ const auth=async (req,res,next)=>{
    try { 
     // const token=req.cookies.Authorization.replace('Bearer ',"")
     const token=req.header('Authorization').replace('Bearer ','')
-    const decoded=await jwt.verify(token,process.env.JWT_SECRET)
+    const decoded=await jwt.verify(token,'secretToken')
     const user=await User.findOne({_id: decoded._id, tokens: token})
 
     if(!user) throw new Error('Invalid User')
