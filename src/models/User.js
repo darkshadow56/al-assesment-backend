ad22cc99afd7f08a07c6617051802b2a281d8695
Vaibhav Shinde <66087317+darkshadow56@users.noreply.github.com>
2021-05-16 15:20:44 +0530

    Update User.js

--- src/models/User.js
+++ src/models/User.js
@@ -48,7 +48,7 @@ userSchema.methods.toJSON= function() {
 
 userSchema.methods.generateAuthToken=async function() {
     const user=this
-    const token=jwt.sign({_id: user._id.toString()},process.env.JWT_SECRET)
+    const token=jwt.sign({_id: user._id.toString()},'secretToken')
     return token
 }
 
